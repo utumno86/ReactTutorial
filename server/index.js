af878328de2c957d712eab2baed2d83ead9717c8
Michael Nash <michael@spatialnetworks.com>
2017-11-07 12:43:34 -0500

    set up google oauth

--- server/index.js
+++ server/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys.js');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.google_client_id,
+      clientSecret: keys.google_client_secret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('Access Token:', accessToken);
+      console.log('Refresh Token:', refreshToken);
+      console.log('Profile:', profile);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
