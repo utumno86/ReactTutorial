42801a80dfecd01a2cb8e4998b5696cb99355372
Michael Nash <michael@spatialnetworks.com>
2017-11-16 17:34:19 -0500

    whoops

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   google_client_secret: process.env.GOOGLE_CLIENT_SECRET,
   google_client_id: process.env.GOOGLE_CLIENT_ID,
-  mongo_uri: process.env.GOOGLE_MONGO_URI,
+  mongo_uri: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 };
