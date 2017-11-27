b4eb7aa332569ee490121e0bd85be5d5b8ea1cb9
Michael Nash <michael@spatialnetworks.com>
2017-11-27 18:22:44 -0500

    react hello world

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App.js';
 
-ReactDOM.render();
+ReactDOM.render(<App />, document.querySelector('#root'));
