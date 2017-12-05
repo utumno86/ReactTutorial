7bd25712ca891f492ac3d47ab65d944ca39f00f9
Michael Nash <michael@spatialnetworks.com>
2017-12-05 17:54:51 -0500

    wired up the thunk actions

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
@@ -7,7 +9,10 @@ const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h1>Landing</h1>;
 
 class App extends Component {
-  componentDidMount() {}
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
   render() {
     return (
       <div className="container">
@@ -24,4 +29,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null, actions)(App);
