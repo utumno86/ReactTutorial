5ef916d286869a3208f8540a483ca4eb442d4877
Michael Nash <michael@spatialnetworks.com>
2018-01-03 18:51:04 -0500

    added stripe config

--- server/config/prod.js
+++ server/config/prod.js
@@ -2,5 +2,7 @@ module.exports = {
   google_client_secret: process.env.GOOGLE_CLIENT_SECRET,
   google_client_id: process.env.GOOGLE_CLIENT_ID,
   mongo_uri: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STIPE_SECRET_KEY
 };
