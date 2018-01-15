9601c87d7cc6b1f714855e62ab8e235340403adc
Michael Nash <michael@spatialnetworks.com>
2018-01-15 16:28:28 -0500

    hooked up the server-side stripe integration

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-
+const bodyParser = require('body-parser')
 const keys = require('./config/keys');
 
 require('./models/User');
@@ -12,6 +12,7 @@ mongoose.connect(keys.mongo_uri);
 
 const app = express();
 
+app.use(bodyParser.json())
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -22,6 +23,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
