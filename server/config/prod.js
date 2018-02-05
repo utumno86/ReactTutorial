aee9858aa15479bb5cf2b54c23c5b9849c8c6f3f
Michael Nash <michael@spatialnetworks.com>
2018-02-05 16:55:47 -0500

    set up sendgrid

--- server/config/prod.js
+++ server/config/prod.js
@@ -4,5 +4,6 @@ module.exports = {
   mongo_uri: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STIPE_SECRET_KEY
+  stripeSecretKey: process.env.STIPE_SECRET_KEY,
+  sendGridKey: process.env.SENDGRID_KEY
 };
