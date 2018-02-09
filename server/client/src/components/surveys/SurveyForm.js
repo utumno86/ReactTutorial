cc5ef0aad27bd20bc879bd5d51c8e11f0cc1eab5
Michael Nash <michael@spatialnetworks.com>
2018-02-09 17:28:27 -0500

    wrote the email address validation

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
 import { Link } from 'react-router-dom'
 import SurveyField from './SurveyField'
+import validateEmails from '../../utils/validateEmails'
 
 const FIELDS = [
   { label: 'Survey Title', name: 'title' },
@@ -36,6 +37,9 @@ class SurveyForm extends Component {
 
 function validate (values) {
   const errors = {}
+
+  errors.emails = validateEmails(values.emails || '')
+  
   _.each(FIELDS, ({ name }) => {
     if (!values[name]) {
       errors[name] = 'You must provide a value!'
