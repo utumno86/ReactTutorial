0d906c4ff36c0e6ad700c625f66861f138bccf61
Michael Nash <michael@spatialnetworks.com>
2018-02-15 18:13:42 -0500

    refactored survey fields as a seperate file

---
+++ server/client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default[
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'line' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipeint List', name: 'emails' }
+]
