6f7a1119f3d7c3ca6ac0466e9c6c2f933292a0e6
Michael Nash <michael@spatialnetworks.com>
2018-02-15 18:28:22 -0500

    wrote survey form review page

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,13 +1,30 @@
 // SurveyFormReview shows user their form inputs for review
+import _ from 'lodash'
 import React from 'react'
 import { connect } from 'react-redux'
+import formFields from './formFields'
 
-const SurveyFormReview = ({ onCancel }) => {
+const SurveyFormReview = ({ onCancel, formValues }) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>
+          {formValues[name]}
+        </div>
+      </div>
+    )
+  })
   return (
     <div>
       <h5>Please confirm your entries</h5>
-      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+      {reviewFields}
+      <button className="yellow white-text darken-3 btn-flat" onClick={onCancel}>
         Back
+      </button>
+      <button className="green btn-flat white-text right">
+        Send Survey
+        <i className="material-icons right">email</i>
       </button>
     </div>
   )
