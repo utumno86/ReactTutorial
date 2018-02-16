7e1c61f6e178a56bb16e1fa2a9e30e2f4bb7301b
Michael Nash <michael@spatialnetworks.com>
2018-02-15 19:13:37 -0500

    fixed a bug in the API call

--- server/client/src/components/surveys/formFields.js
+++ server/client/src/components/surveys/formFields.js
@@ -1,6 +1,6 @@
 export default[
   { label: 'Survey Title', name: 'title' },
-  { label: 'Subject Line', name: 'line' },
+  { label: 'Subject Line', name: 'subject' },
   { label: 'Email Body', name: 'body' },
   { label: 'Recipeint List', name: 'recipients' }
 ]
--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -13,7 +13,6 @@ module.exports = app => {
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body
-
     const survey = new Survey({
       title,
       subject,
