9fe5a280ce4775b67bc62b2cc62a35d456a67dd6
Michael Nash <michael@spatialnetworks.com>
2018-02-15 20:32:59 -0500

    wrote logic to redirect to dashboard when surveys are sent

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -2,10 +2,11 @@
 import _ from 'lodash'
 import React from 'react'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 import formFields from './formFields'
 import * as actions from '../../actions'
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
   const reviewFields = _.map(formFields, ({ name, label }) => {
     return (
       <div key={name}>
@@ -23,7 +24,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
       <button className="yellow white-text darken-3 btn-flat" onClick={onCancel}>
         Back
       </button>
-      <button className="green btn-flat white-text right" onClick={() => submitSurvey(formValues)}>
+      <button className="green btn-flat white-text right" onClick={() => submitSurvey(formValues, history)}>
         Send Survey
         <i className="material-icons right">email</i>
       </button>
@@ -35,4 +36,4 @@ function mapStateToProps (state) {
   return { formValues: state.form.surveyForm.values }
 }
 
-export default connect(mapStateToProps, actions)(SurveyFormReview)
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview))
